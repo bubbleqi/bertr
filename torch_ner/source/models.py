8cdbebaee36d0e1f5148d6f404830220b8cd5d42
zchen <chenzhouwy@163.com>
2021-07-06 10:11:22 +0800

    fix some code smell

--- torch_ner/source/models.py
+++ torch_ner/source/models.py
@@ -12,18 +12,11 @@ from torchcrf import CRF
 class BERT_BiLSTM_CRF(BertPreTrainedModel):
 
     def __init__(self, config, need_birnn=False, rnn_dim=128):
-        """
-        初始化所需要的模型，包括BertModel、RNN、Linear、CRF。
-        :param config: 模型参数
-        :param need_birnn: 是否需要RNN
-        :param rnn_dim:  输入数据的特征维数
-        """
         super(BERT_BiLSTM_CRF, self).__init__(config)
 
         """
         1. BERT:
             outputs = self.bert(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=input_mask)
-            # torch.Size([batch_size,seq_len,hidden_size]) --- [6,128,768]
             sequence_output = outputs[0]
             
             Inputs:
@@ -31,7 +24,7 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
                 token_type_ids: torch.Size([batch_size,seq_len]), 一个实例可以含有两个句子,相当于标记
                 attention_mask: torch.Size([batch_size,seq_len]), 指定对哪些词进行self-Attention操作
             Out:
-                sequence_output: torch.Size([batch_size,seq_len,hidden_size]), 输出序列
+                sequence_output: torch.Size([batch_size,seq_len,hidden_size]), 输出序列---[6,128,768]
                 pooled_output:   torch.Size([batch_size,hidden_size]), 对输出序列进行pool操作的结果
                 (hidden_states): tuple, 13*torch.Size([batch_size,seq_len,hidden_size]), 隐藏层状态，取决于config的output_hidden_states
                 (attentions):    tuple, 12*torch.Size([batch_size, 12, seq_len, seq_len]), 注意力层，取决于config中的output_attentions
@@ -42,7 +35,6 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
 
         """
         2. BiLSTM:
-            # input_size:768, hidden_size=128
             self.birnn = nn.LSTM(input_size=config.hidden_size, hidden_size=rnn_dim, num_layers=1, bidirectional=True, batch_first=True)
             Args:
                 input_size:    输入数据的特征维数
@@ -53,7 +45,6 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
                 dropout:       默认是0，代表不用dropout
                 bidirectional: 默认是false，代表不用双向LSTM
             
-            # [6,128,768] --> [6,128,256]    
             sequence_output, _ = self.birnn(sequence_output)
             Inputs:
                 input:     shape=(seq_length,batch_size,input_size)的张量
@@ -61,7 +52,7 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
                            num_layers就是LSTM的层数，如果bidirectional=True,num_directions=2,否则就是１，表示只有一个方向，c_0和h_0的形状相同，
                            它包含的是在当前这个batch_size中的每个句子的初始状态，h_0、c_0如果不提供，那么默认是０
             OutPuts:
-                output:    shape=(seq_length,batch_size,num_directions*hidden_size), 它包含的LSTM的最后一层的输出特征(h_t),t是batch_size中每个句子的长度
+                output:    shape=(seq_length,batch_size,num_directions*hidden_size), 它包含的LSTM的最后一层的输出特征(h_t),t是batch_size中每个句子的长度--- [6,128,256] 
                 (h_n,c_n): h_n.shape=(num_directions * num_layers,batch,hidden_size), c_n与H_n形状相同, h_n包含的是句子的最后一个单词的隐藏状态，
                            c_n包含的是句子的最后一个单词的细胞状态，所以它们都与句子的长度seq_length无关;output[-1]与h_n是相等的，因为output[-1]包含的正是batch_size
                            个句子中每一个句子的最后一个单词的隐藏状态，注意LSTM中的隐藏状态其实就是输出，cell state细胞状态才是LSTM中一直隐藏的，记录着信息
@@ -77,7 +68,7 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
             self.hidden2tag = nn.Linear(in_features=out_dim, out_features=config.num_labels)
             Args:
                 in_features:  输入的二维张量的大小，即输入的[batch_size, size]中的size
-                out_features: 输出的二维张量的大小，即输出的二维张量的形状为[batch_size，output_size]，当然，它也代表了该全连接层的神经元个数
+                out_features: 输出的二维张量的大小，即输出的二维张量的形状为[batch_size，output_size]，当然，它也代表了该全连接层的神经元个数---[6,128,11] 
 
             释义:
                 从输入输出的张量的shape角度来理解，相当于一个输入为[batch_size, in_features]的张量变换成了[batch_size, out_features]的输出张量
@@ -105,6 +96,7 @@ class BERT_BiLSTM_CRF(BertPreTrainedModel):
                     ``none|sum|mean|token_mean``. ``none``: no reduction will be applied.
                     ``sum``: the output will be summed over batches. ``mean``: the output will be
                     averaged over batches. ``token_mean``: the output will be averaged over tokens.
+                    
             Returns:
             `~torch.Tensor`: The log likelihood. This will have size ``(batch_size,)`` if
             reduction is ``none``, ``()`` otherwise.
